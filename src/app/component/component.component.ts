a00566994edb345c334b44a1f412d32040424535
Samiksha Gautam <samikshagautam@Samikshas-MacBook-Air.local>
2024-01-03 15:21:57 -0800

    first commit

---
+++ src/app/component/component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  templateUrl: './component.component.html',
+  styleUrl: './component.component.css'
+})
+export class ComponentComponent {
+
+}
