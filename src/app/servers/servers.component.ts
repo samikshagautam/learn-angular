09333b9835c4ecd3e4fc38c7e707373fca58a956
Samiksha Gautam <samikshagautam@Samikshas-MacBook-Air.local>
2024-01-03 16:14:18 -0800

    Directives ngIf, ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit{
   serverCreationStatus = "No server was created!";
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit{
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
